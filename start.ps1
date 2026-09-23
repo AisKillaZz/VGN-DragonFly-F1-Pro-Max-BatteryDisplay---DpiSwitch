354061ddd8c52af9f733392c9806c6b947c6824f
AisKillaZz <91125261+AisKillaZz@users.noreply.github.com>
2026-09-23 14:34:50 +0800

    Delete start.ps1

--- start.ps1
+++
@@ -1,9 +0,0 @@
-[CmdletBinding()]
-param()
-
-$ErrorActionPreference = 'Stop'
-$exe = Join-Path $PSScriptRoot 'bin\Debug\net8.0-windows\VgnTrayBattery.exe'
-if (-not (Test-Path -LiteralPath $exe)) {
-    throw '还没有构建程序。请先运行 .\build.ps1'
-}
-Start-Process -FilePath $exe -WorkingDirectory (Split-Path -Parent $exe)
